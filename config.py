245b6cf1ae976c8775bea4b30e32bd2921f64e7e
zaidpsdev <167524541+zaidpsdev@users.noreply.github.com>
2024-04-19 13:10:21 +0300

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
+APP_ID = os.environ.get("27455984")
+APP_HASH = os.environ.get("62d5f68ce2e9189636967120220f5755")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 session = os.environ.get("TERMUX")
 SESSION = os.environ.get("TERMUX")
